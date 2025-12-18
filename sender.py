bf2bdf10fa8795de6add47668964a5d39955cbd9
Kamile GÜLER <kamile.guler32@gmail.com>
2025-12-18 15:24:50 +0300

    sender düzenlendi

--- sender.py
+++ sender.py
@@ -1,12 +1,23 @@
 import socket
 from error_methods import calculate_checksum
+import random
 
 SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 5000
 
+SUPPORTED_METHODS = [
+    "PARITY",
+    "PARITY_ODD",
+    "2D_PARITY",
+    "CRC16",
+    "CRC32",
+    "INTERNET_CHECKSUM",
+]
 
-def create_packet(message: str, method: str = "PARITY") -> tuple[str, str, str]:
-    
+def create_packet(message: str, method: str ) -> tuple[str, str, str]:
+    method = method.strip().upper()
+    if "|" in message:
+        raise ValueError("Message cannot contain the '|' character.")
     data_bytes = message.encode("utf-8")
     checksum = calculate_checksum(data_bytes, method)
     packet_str = f"{message}|{method}|{checksum}"
@@ -31,8 +42,11 @@ def send_packet_to_server(packet_str: str, host: str = SERVER_HOST, port: int = 
 def main():
     data_str = input("Enter the message to send: ")
 
-    packet_str, method, checksum = create_packet(data_str)
-    print(f"Packet to be sent: {packet_str}")
+    method = random.choice(SUPPORTED_METHODS)
+    packet_str, method, checksum = create_packet(data_str, method)
+
+    print(f"Chosen METHOD     : {method}")
+    print(f"Packet to be sent : {packet_str}")
 
     if send_packet_to_server(packet_str):
         print("Packet sent to server.")
